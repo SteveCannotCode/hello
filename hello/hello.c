771bd9b24958b1afe0812046a8616d286e853db5
Steven Le <sle5@huskers.unl.edu>
2021-08-30 19:12:15 +0000

    initial commit

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Steven Le
+ * Date: 2021/8/25
+ *
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Steven\n");
+
+    return 0;
+}
